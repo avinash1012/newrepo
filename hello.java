828128c5c2266cdcbacba1b646617723c69ef1e4
avinash1012 <87105887+avinash1012@users.noreply.github.com>
2021-07-13 23:21:21 +0530

    all is well
    all is well

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 <<<<<<< HEAD
 System.out.println("hello all the best");
 =======
-System.out.println("hello god bless you");
+System.out.println("hello god bless you all is well");
 >>>>>>> child1
 }
 }
